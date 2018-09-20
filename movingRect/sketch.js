61d9ac797d8bb23b37452e951195674a4dbccb76
Tylerdives <43014225+Tylerdives@users.noreply.github.com>
2018-09-20 13:42:49 -0600

    redone moving rect

--- movingRect/sketch.js
+++ movingRect/sketch.js
@@ -5,10 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x;
+let dx;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width/2;
+  dx = 3;
 }
 
 function draw() {
-
+  background(255);
+  fill(45, 50, 175);
+  rect(x, height/2, 100, 50);
+  if (x + 100 > width || x < 0){
+    dx = dx * -1;
+  }
+  x += dx;
 }
