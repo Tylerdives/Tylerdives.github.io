56e19a797c76db9555f4c751dd642b64c2b1a459
Tylerdives <43014225+Tylerdives@users.noreply.github.com>
2018-11-15 14:16:21 -0600

    stuffs

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,8 +1,8 @@
-// Fireworks
+// ball
 // Tlyer Boechler
 // November 14th, 2018
 
-class Particle {
+class Ball {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -10,7 +10,7 @@ class Particle {
     this.dx = random(-10, 10);
     this.dy = random(-10, 10);
     // this.transparency = 255;
-    this.color = color(random(255), random(255), random(255));
+    this.color = color(random(255), random(255), random(255), 80);
   }
 
   display() {
@@ -22,14 +22,14 @@ class Particle {
   update() {
     // this.transparency -= 4;
     // this.color.setAlpha(this.transparency);
-    if(this.x < 0 + this.size/2 || this.x > width - this.size/2) {
+    if (this.x < 0 + this.size/2 || this.x > width - this.size/2) {
       this.dx = this.dx * -1;
     }
-    if(this.y < 0 + this.size/2 || this.y > height - this.size/2) {
+    if (this.y < 0 + this.size/2 || this.y > height - this.size/2) {
       this.dy = this.dy * -1;
     }
 
-    
+
 
     this.x += this.dx;
     this.y += this.dy;
@@ -37,7 +37,7 @@ class Particle {
 }
 
 let someParticle;
-let fireworks = [];
+let ball = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -46,13 +46,13 @@ function setup() {
 function draw() {
   background(0);
 
-  for(let i = 0; i< fireworks.length; i++) {
-    fireworks[i].update();
-    fireworks[i].display();
+  for(let i = ball.length-1; i>=0; i--) {
+    ball[i].update();
+    ball[i].display();
   }
 }
 
 function mousePressed() {
-  let someParticle = new Particle(mouseX, mouseY);
-  fireworks.push(someParticle);
+  let someParticle = new Ball(mouseX, mouseY);
+  ball.push(someParticle);
 }
