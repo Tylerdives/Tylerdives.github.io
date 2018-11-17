1dba3fd34f7dfd0dcd77579d0c820691d67215c5
Tylerdives <43014225+Tylerdives@users.noreply.github.com>
2018-11-16 20:45:44 -0600

    drop logic

--- weathersim/sketch.js
+++ weathersim/sketch.js
@@ -8,10 +8,10 @@
 class Raindrop  {
   constructor() {
     this.x = random(width);
-    this.y = random(-1000, -10);
+    this.y = random(-2000, -10);
     this.dx = 0;
     this.dy = 8;
-    this.radius = 6;
+    this.radius = random(3, 6);
     this.color = color(0, random(60), 255 - random(50), 255 - random(150));
     this.touchingGround = false;
   }
@@ -23,17 +23,17 @@ class Raindrop  {
   }
 
   fall() {
-    if (this.y + this.dy >= collectionHeight) {
+    if (this.y + this.dy >= collectionHeight + this.radius/2) {
       this.touchingGround = true;
-
+      this.y = random(-1000, -10);
+      this.dy = 8;
     }
     else {
-      this.dy += 0.3;
+      this.dy += 0.2;
       this.y += this.dy;
     }
 
   }
-
 }
 
 let rainArray = [];
@@ -42,15 +42,20 @@ let collectionHeight;
 let buffer = 31;
 
 let weather;
+let noRain;
+let noSnow;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   weather = "rain";
+  noRain = true;
 }
 
 function draw() {
   if (weather === "rain") {
-
+    if (noRain) {
+      generateRain();
+    }
     displayRain();
   }
 }
@@ -58,12 +63,11 @@ function draw() {
 function mousePressed() {
   //rain
   generateRain();
-
-
 }
 
 function displayRain() {
-  collectionHeight = height - dropCounter * 0.1;
+
+  collectionHeight = height - dropCounter * 0.0001;
   background(255);
   for (let i=rainArray.length-1; i>0; i--) {
     rainArray[i].fall();
@@ -77,15 +81,16 @@ function displayRain() {
 function touchingGround(i) {
   if (rainArray[i].touchingGround) {
     dropCounter++;
-    rainArray.splice(i, 1);
+    // rainArray.splice(i, 1);
   }
 }
 
 function generateRain() {
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < 400; i++) {
     let someRain = new Raindrop();
     rainArray.push(someRain);
   }
+  noRain = false;
 }
 
 
@@ -95,5 +100,5 @@ function elementalCollection(element) {
     fill(0, 0, 255, 150);
   }
 
-  rect(0, collectionHeight, width, dropCounter * 0.1);
+  rect(0, collectionHeight, width, dropCounter * 0.0001);
 }
