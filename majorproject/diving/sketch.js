b5622ebd9294910a39a7c7375b3e1aaafaf9dd06
Tylerdives <43014225+Tylerdives@users.noreply.github.com>
2018-12-10 14:12:26 -0600

    Rotations are pains

--- majorproject/diving/sketch.js
+++ majorproject/diving/sketch.js
@@ -2,15 +2,64 @@
 // Tyler Boechler
 // December 12th, 2018
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
+class Player {
+  constructor(boardHeight) {
+    this.x = width/2;
+    this.y = height/2;
+    this.dx = 0.2;
+    this.dy = 1;
+
+    this.bodyColor = color(255, 0, 0);
+    this.headColor = color(0, 255, 0);
+    this.feetColor = color(255, 255, 0);
+
+    this.angle = 0;
+
+    this.position = "straight";
+  }
+
+  display() {
+    fill(this.bodyColor);
+    rect(this.x, this.y, 50, 150);
+    fill(this.headColor);
+    rect(this.x, this.y, 50, 20);
+    fill(this.feetColor);
+    rect(this.x, this.y + 130, 50, 20);
+  }
+
+  fall() {
+
+  }
+
+  rotatePlayer() {
+    translate(this.x, this.y);
+    rotate(1);
+
+  }
+}
+
+let player;
 
 
 function setup() {
+  angleMode(DEGREES);
   createCanvas(windowWidth, windowHeight);
+
+  player = new Player(0);
 }
 
 function draw() {
+  drawPlayer();
+  updatePlayer();
+
+}
+
+function drawPlayer() {
+  player.display();
+}
+
+function updatePlayer() {
+  player.rotatePlayer();
 
 }
